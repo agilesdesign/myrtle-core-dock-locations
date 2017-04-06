1184f3a0251167b0fcc4cd1bf4322d2345416b22
Justin Seliga <jseliga@agilesdesign.com>
2017-04-06 12:16:30 -0400

    namespacing fixes

--- src/Locations/Models/Location.php
+++ src/Locations/Models/Location.php
@@ -4,7 +4,7 @@ namespace Myrtle\Core\Locations\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Myrtle\Phones\Models\Traits\Phoneable;
+use Myrtle\Core\Phones\Models\Traits\Phoneable;
 use Myrtle\Core\Addresses\Models\Traits\Addressable;
 
 class Location extends Model {
